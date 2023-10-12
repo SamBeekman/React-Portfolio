11cfcf8cb12ca9670f8167380864305ca145cccc
SamBeekman <sambeekman@hotmail.com>
2023-10-12 20:24:19 +1100

    changed directory

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,7 +2,7 @@ function Project(props) {
 
 
     return <>
-        <div class="item">
+        <div className="item">
             <h3><a href={props.github}>{props.title}</a></h3><a href={props.deployed}><img id="image" src={props.image} alt={props.altText}></img></a>
         </div>
     </>
